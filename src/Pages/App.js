ee93aaf080cc691bd1050076b45dcbb660db3c49
Ntshangase <simphiwe323i@gmail.com>
2024-01-29 16:12:50 +0200

    react router

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,8 +1,10 @@
 import '../Styles/App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       
       <p>Where will this be</p>
     </div>
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/Nav.css';
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <ul>
+            <li>tab1</li>
+            <li>tab2</li>
+            <li>tab3</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
